86eb994f87cf75552f363db0b5eb0c8292b5fec8
mohanraj v <mohanrajvengi@gmail.com>
2020-11-20 17:51:32 +0530

    QA_Check

--- database/Database.js
+++ database/Database.js
@@ -1,30 +1,27 @@
 //Import Package
 const Mongoose = require("mongoose");
 const Env = require("dotenv").config({ path: require("find-config")(".env") });
-const DB_HOST=Env.parsed.DB_HOST;
+
 //Database Connection
 exports.InitilizeDatabase = async () => {
     try {
-        await Mongoose.connect(
-            DB_HOST,
-            {
-                useUnifiedTopology: true,
-                useNewUrlParser: true,
-                useFindAndModify: true,
-                useCreateIndex: true,
-                useFindAndModify: false,
-            }
-        );
+        await Mongoose.connect(process.env.DB_HOST, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            useFindAndModify: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+        });
         console.log(
             new Date().toString() +
-                DB_HOST.slice(30).padStart(93, "*") +
+                Env.parsed.DB_HOST.slice(30).padStart(93, "*") +
                 " Database connected"
         );
     } catch (error) {
         console.log(
             new Date().toString() +
                 " Unable to Connect " +
-                DB_HOST.slice(30).padStart(93, "*")
+                Env.parsed.DB_HOST.slice(30).padStart(93, "*")
         );
         console.log(error);
         throw error;
